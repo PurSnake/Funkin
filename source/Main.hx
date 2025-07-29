253c6d92b67b8f4824d20947d7fbe4e9b1842e7f
PurSnake <mikesaygasamp@mail.ru>
2025-07-30 00:05:42 +0300

    bump

--- source/Main.hx
+++ source/Main.hx
@@ -14,28 +14,33 @@ import hxvlc.util.Handle;
 #end
 import openfl.display.FPS;
 import openfl.display.Sprite;
+import openfl.display.MovieClip;
 import openfl.events.Event;
 import openfl.Lib;
 import openfl.media.Video;
 import openfl.net.NetStream;
 import funkin.util.WindowUtil;
 
-/**
- * The main class which initializes HaxeFlixel and starts the game in its initial state.
- */
-class Main extends Sprite
+class Main extends flixel.FlxGame
 {
-  var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-  var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-  var initialState:Class<FlxState> = funkin.InitState; // The FlxState the game starts with.
-  var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-  var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-  var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	public static var mainInstance(default, null):Sprite;
+	public static var applicationScreen(get, never):MovieClip;
 
-  // You can pretty much ignore everything from here on - your code should go in your states.
+	@:noCompletion inline static function get_applicationScreen()
+		return Lib.current;
 
-  public static function main():Void
-  {
+  /**
+   * A frame counter displayed at the top left.
+   */
+  public static var fpsCounter:FPS;
+
+  /**
+   * A RAM counter displayed at the top left.
+   */
+  public static var memoryCounter:MemoryCounter;
+
+	public static function main():Void
+	{
     // Set the current working directory for Android and iOS devices
     #if android
     // On Android use External Files Dir.
@@ -48,58 +53,11 @@ class Main extends Sprite
     // We need to make the crash handler LITERALLY FIRST so nothing EVER gets past it.
     CrashHandler.initialize();
     CrashHandler.queryStatus();
+		mainInstance = new Main();
+	}
 
-    Lib.current.addChild(new Main());
-  }
-
-  public function new()
-  {
-    super();
-
-    // Initialize custom logging.
-    haxe.Log.trace = funkin.util.logging.AnsiTrace.trace;
-    funkin.util.logging.AnsiTrace.traceBF();
-
-    // Load mods to override assets.
-    // TODO: Replace with loadEnabledMods() once the user can configure the mod list.
-    funkin.modding.PolymodHandler.loadAllMods();
-
-    if (stage != null)
-    {
-      init();
-    }
-    else
-    {
-      addEventListener(Event.ADDED_TO_STAGE, init);
-    }
-  }
-
-  function init(?event:Event):Void
-  {
-    if (hasEventListener(Event.ADDED_TO_STAGE))
-    {
-      removeEventListener(Event.ADDED_TO_STAGE, init);
-    }
-
-    setupGame();
-  }
-
-  var video:Video;
-  var netStream:NetStream;
-  var overlay:Sprite;
-
-  /**
-   * A frame counter displayed at the top left.
-   */
-  public static var fpsCounter:FPS;
-
-  /**
-   * A RAM counter displayed at the top left.
-   */
-  public static var memoryCounter:MemoryCounter;
-
-  function setupGame():Void
-  {
+	public function new()
+	{
     initHaxeUI();
 
     // addChild gets called by the user settings code.
@@ -116,8 +74,10 @@ class Main extends Sprite
     FlxG.signals.preUpdate.add(repositionCounters.bind(true));
     #end
 
-    // George recommends binding the save before FlxGame is created.
     Save.load();
+
+    haxe.Log.trace = funkin.util.logging.AnsiTrace.trace;
+    funkin.modding.PolymodHandler.loadAllMods();
 
     #if hxvlc
     // Initialize hxvlc's Handle here so the videos are loading faster.
@@ -133,18 +93,20 @@ class Main extends Sprite
 
     WindowUtil.setVSyncMode(funkin.Preferences.vsyncMode);
 
-    var game:FlxGame = new FlxGame(gameWidth, gameHeight, initialState, Preferences.framerate, Preferences.framerate, skipSplash, startFullscreen);
 
-    // FlxG.game._customSoundTray wants just the class, it calls new from
-    // create() in there, which gets called when it's added to the stage
-    // which is why it needs to be added before addChild(game) here
-    @:privateAccess
-    game._customSoundTray = funkin.ui.options.FunkinSoundTray;
+		super(1280, 720, funkin.InitState, funkin.Preferences.framerate, funkin.Preferences.framerate, true, false);
+    _customSoundTray = funkin.ui.options.FunkinSoundTray;
+    
+		scrollRect = new openfl.geom.Rectangle();
+		__scrollRect.setTo(0, 0, FlxG.scaleMode.gameSize.x, FlxG.scaleMode.gameSize.y);
+		applicationScreen.addChild(this);
 
-    addChild(game);
+		applicationScreen.stage.scaleMode = openfl.display.StageScaleMode.NO_SCALE;
+
+		FlxG.signals.gameResized.add((w, h) -> __scrollRect.setTo(0, 0, FlxG.scaleMode.gameSize.x, FlxG.scaleMode.gameSize.y));
 
     #if FEATURE_DEBUG_FUNCTIONS
-    game.debugger.interaction.addTool(new funkin.util.TrackerToolButtonUtil());
+    debugger.interaction.addTool(new funkin.util.TrackerToolButtonUtil());
     #end
 
     #if !html5
@@ -161,7 +123,24 @@ class Main extends Sprite
     #else
     trace('hxcpp_debug_server is disabled! This build does not support debugging.');
     #end
-  }
+	}
+
+	var skipNextTickUpdate:Bool = false;
+
+	public override function switchState()
+	{
+		super.switchState();
+		draw();
+		_total = ticks = getTicks();
+		skipNextTickUpdate = true;
+	}
+
+	public override function onEnterFrame(t)
+	{
+		if (skipNextTickUpdate != (skipNextTickUpdate = false))
+			_total = ticks = getTicks();
+		super.onEnterFrame(t);
+	}
 
   function initHaxeUI():Void
   {
@@ -178,6 +157,7 @@ class Main extends Sprite
     haxe.ui.tooltips.ToolTipManager.defaultDelay = 200;
   }
 
+  
   #if mobile
   function repositionCounters(lerp:Bool):Void
   {
